3901dc30f559ca65b0494515b36e0eff8751268f
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-05-30 11:18:20 +0100

    Small gui updates

--- base.py
+++ base.py
@@ -1,12 +1,31 @@
 from tkinter import *
+from book_info import read_a_quote, clear_quotes
+
+
+def search_query():
+    selector = read_a_quote(query.get())
+    results(selector)
+
+
+def results(packer):
+    answer = str(packer)
+    label = Label(frame, text=answer)
+    label.grid(row=1, column=0)
+
 
 root = Tk()
-
 root.title("Quotes storage")
+frame = Frame(root, width=800, height=300, background="bisque")
+frame.grid(row=0, column=0)
+query = Entry(frame)
+query.grid(row=0, column=0, ipadx=12, ipady=10)
 
 
-def gui(packer):
-    top = Toplevel()
-    label = Label(top, text=packer)
-    label.pack()
-    top.title("These are the quotes")
+searcher = Button(frame, width=12, height=2, text="search for a book", command=lambda: search_query())
+clearer = Button(frame, width=12, height=2, text="Clear all quotes", command=lambda: clear_quotes())
+clearer.grid(row=1, column=1)
+searcher.grid(row=0, column=1)
+
+
+
+
--- main.py
+++ main.py
@@ -3,27 +3,7 @@ This is an application which allows the user to enter book quotes into a file an
 title to find all quotes of that sort at a later date.
 """
 
-from book_info import Book, read_a_quote, clear_quotes
 from base import *
 
-read_or_write = input("Do you want to clear all quotes, read or write a quote?: ")
-global adder
-if read_or_write == "write":
-    title = input("What is the title of the book?: ")
-    author = input("What is the name of the author?: ")
-    quote = input("Please write the desired quote: ")
-    adder = Book(title, author, quote)
-    adder.write_a_quote()
-
-elif read_or_write == "read":
-    search = input("What is: the name of the author or the name of the book: ")
-    gui(read_a_quote(search))
-
-elif read_or_write == "clear":
-    sure = input("Are you sure you want to delete all messages?: ")
-    if sure == "yes":
-        clear_quotes()
-    else:
-        print("Thankyou for using the application")
 
 mainloop()
